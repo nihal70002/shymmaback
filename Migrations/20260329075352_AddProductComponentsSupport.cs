1aa1017e254d0478d13766f004ee238d705e9654
nihal70002 <nihalahd7000@gmail.com>
2026-03-29 13:25:13 +0530

    odppppppppd

---
+++ Migrations/20260329075352_AddProductComponentsSupport.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ClientEcommerce.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductComponentsSupport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductComponents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CatNo = table.Column<string>(type: "text", nullable: true),
+                    InstrumentName = table.Column<string>(type: "text", nullable: true),
+                    Units = table.Column<int>(type: "integer", nullable: false),
+                    ProductId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductComponents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductComponents_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductComponents_ProductId",
+                table: "ProductComponents",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductComponents");
+        }
+    }
+}
